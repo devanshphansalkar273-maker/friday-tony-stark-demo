16d46b0e001e1d4494491ba38c20ea55b84ef918
devanshphansalkar273-maker <devanshphansalkar273@gmail.com>
2026-04-20 15:44:39 +0530

    work automatically

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 from core.agent import handle_input
-from voice.output import speak
-from voice.input import listen
 
 def main():
-    print("FRIDAY is running...", flush=True)
+    print("FRIDAY is running...")
 
     while True:
-        user_input = listen()
-
-        if not user_input:
-            continue
-
-        print("You:", user_input, flush=True)
+        user_input = input("You: ")
 
         response = handle_input(user_input)
 
-        print("FRIDAY:", response, flush=True)
-        speak(response)
+        print("FRIDAY:", response)
 
 if __name__ == "__main__":
     main()
---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d C:\Users\Croma\OneDrive\Desktop\Documents\GitHub\friday-tony-stark-demo
+call venv\Scripts\activate
+python main.py
+pause
